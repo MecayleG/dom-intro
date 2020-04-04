7a3f8e627656c72a99fb0859457aa4607907201d
MecayleG <mecayle@DESKTOP-TP3QTRD.localdomain>
2020-04-04 19:11:57 +0200

    made buttons work

--- js/radio-bill.js
+++ js/radio-bill.js
@@ -10,3 +10,9 @@
 // * add the appropriate value to the running total
 // * add nothing for invalid values that is not 'call' or 'sms'.
 // * display the latest total on the screen
+const billItemTypeRadio  = document.querySelector(".billItemTypeRadio");
+const radioBillBtn = document.querySelector(".radioBillAddBtn");
+const callTotalTwo = document.querySelector(".callTotalTwo");
+const smsTotalTwo = document.querySelector(".smsTotalTwo");
+const totalTwo = document.querySelector(".totalTwo");
+
--- js/text-bill.js
+++ js/text-bill.js
@@ -10,3 +10,32 @@
 // * add the appropriate value to the running total
 // * add nothing for invalid values that is not 'call' or 'sms'.
 // * display the latest total on the screen
+const billTypeText = document.querySelector(".billTypeText");
+const textTotalAddBtn = document.querySelector(".addToBillBtn");
+const callsTotalElem = document.querySelector(".callTotalOne");
+const smsTotalElem = document.querySelector(".smsTotalOne");
+const totalCostElem = document.querySelector(".totalOne");
+
+function textBillTotal(){
+    var billTypeEntered = billTypeText.value.trim();
+    if (billTypeEntered === "call"){
+        callsTotal += 2.75
+    }
+    else if (billTypeEntered === "sms"){
+        smsTotal += 0.75;
+    }
+    callsTotalElem.innerHTML = callsTotal.toFixed(2);
+    smsTotalElem.innerHTML = smsTotal.toFixed(2);
+    var totalCost = callsTotal + smsTotal;
+    totalCostElem.innerHTML = totalCost.toFixed(2);
+    if (totalCost >= 50){
+        totalCostElem.classList.add("danger");
+    }
+    else if (totalCost >= 30){
+        totalCostElem.classList.add("warning");
+    }
+}
+var callsTotal = 0;
+var smsTotal = 0;
+textTotalAddBtn.addEventListener('click', textBillTotal);
+
