2c826b5b1beb0f5c2815bba708eedbede01a3851
MecayleG <mecayle@DESKTOP-TP3QTRD.localdomain>
2020-04-07 17:42:08 +0200

    updated features to work

--- js/settings-bill.js
+++ js/settings-bill.js
@@ -23,3 +23,83 @@
 // * check the value thresholds and display the total value in the right color.
 
 
+const callCost = document.querySelector(".callCostSetting");
+const smsCost = document.querySelector(".smsCostSetting");
+const warningLevel = document.querySelector(".warningLevelSetting ");
+const criticalLevel = document.querySelector(".criticalLevelSetting");
+const updateSettingsBtn = document.querySelector(".updateSettings");
+
+const radioTypeItem = document.querySelector(".billItemTypeWithSettings");
+const settingsAddBtn = document.querySelector(".settingsAdd");
+
+
+const radioCallTotalSettings = document.querySelector(".callTotalSettings");
+const radioSmsTotalSettings = document.querySelector(".smsTotalSettings");
+const radioTotalSettings = document.querySelector(".totalSettings");
+
+var totalForCalls = 0;
+var totalForSms = 0;
+var allTotals = 0;
+var callCostNew = 0;
+var smsCostNew = 0;
+var warningLevelNew = 0;
+var criticalLevelNew = 0;
+
+function firstAdd(billItemTypeTwo){
+	
+	if(billItemTypeTwo === 'call'){
+		totalForCalls += callCostNew;
+		allTotals += callCostNew;
+	}
+	else if (billItemTypeTwo === 'sms'){
+		totalForSms += smsCostNew;
+		allTotals += smsCostNew;
+	}
+	
+	var forDecimals = allTotals.toFixed(2);
+	
+	return forDecimals;
+	}
+
+
+ function forColor(allTotalsTwo){
+ 	const colorTotals = Number(allTotalsTwo);
+
+    radioTotalSettings.classList.remove("danger");
+    radioTotalSettings.classList.remove("warning");
+
+    if(colorTotals >= warningLevelNew  && colorTotals < criticalLevelNew){
+    	radioTotalSettings.classList.add("warning");
+    } else if (colorTotals > criticalLevelNew) {
+		radioTotalSettings.classList.add("danger");
+    }
+ }
+
+ function settingsTotal(){
+ 	totalForCalls = 0;
+ 	totalForSms = 0;
+ 	allTotals = 0;
+  callCostNew = Number(callCost.value);
+  smsCostNew = Number(smsCost.value);
+  warningLevelNew = Number(warningLevel.value);
+  criticalLevelNew = Number(criticalLevel.value);
+ }
+function callingAll(){
+	var checkedRadioBtnTwo = document.querySelector("input[name='billItemTypeWithSettings']:checked");
+	var radioItemType = checkedRadioBtnTwo.value;
+	firstAdd(radioItemType);
+	forColor(allTotals);
+	// alert(allTotals);
+	// alert(totalForCalls);
+	// alert(totalForSms);
+	radioTotalSettings.innerHTML = allTotals.toFixed(2);
+	radioCallTotalSettings.innerHTML = totalForCalls.toFixed(2);
+	radioSmsTotalSettings.innerHTML = totalForSms.toFixed(2);
+}
+updateSettingsBtn.addEventListener('click', settingsTotal);
+settingsAddBtn.addEventListener('click', callingAll);
+
+
+
+
+
